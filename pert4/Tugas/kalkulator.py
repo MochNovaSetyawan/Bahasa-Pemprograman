eebc19ca9077a4c43566701fafb598f74695bd0b
root <root@Chrome>
2024-04-27 09:09:32 +0700

    (Perubahan pada 27/04/2024 fix3)

--- pert4/Tugas/kalkulator.py
+++ pert4/Tugas/kalkulator.py
@@ -1,3 +1,17 @@
+def tambah(x, y):
+    return x + y
+
+def kurang(x, y):
+    return x - y
+
+def kali(x, y):
+    return x * y
+
+def bagi(x, y):
+    if y == 0:
+        return "Error: Pembagian dengan nol"
+    return x / y
+
 def hitung(pilihan, value1, value2):
     if pilihan == '1':
         return tambah(value1, value2)
@@ -10,12 +24,11 @@ def hitung(pilihan, value1, value2):
     
 def tampilan_info():
     print("Operasi:")
-    print("1.Tambah")
-    print("2.Kurang")
-    print("3.Kali")
-    print("4.Bagi")
-    print("5.Keluar")
-
+    print("1. Tambah")
+    print("2. Kurang")
+    print("3. Kali")
+    print("4. Bagi")
+    print("5. Keluar")
 
 def main():
     while True:
@@ -23,16 +36,19 @@ def main():
         pilihan = input("Pilih operasi (1/2/3/4/5): ")
 
         if pilihan in ('1', '2', '3', '4'):
-            value1 = float(input("Masukkan angka pertama: "))
-            value2 = float(input("Masukkan angka kedua: "))
-            hasil = hitung(pilihan, value1, value2)
-            print("Hasil: ", hasil)
+            try:
+                value1 = float(input("Masukkan angka pertama: "))
+                value2 = float(input("Masukkan angka kedua: "))
+                hasil = hitung(pilihan, value1, value2)
+                print("Hasil: ", hasil)
+            except ValueError:
+                print("Input tidak valid. Harap masukkan angka.")
         elif pilihan == '5':
-            print("Quit")
+            print("Keluar dari program...")
             break
         else:
-            print("Pilihan tidak valid!")        
-
+            print("Pilihan tidak valid!")
 
 if __name__ == "__main__":
-    main()            
+    main()
+         

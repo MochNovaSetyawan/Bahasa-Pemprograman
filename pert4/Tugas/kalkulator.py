6cbfcef69682e53e29a5fdf7b58f224fc8d8d7ac
root <root@Chrome>
2024-04-27 09:05:03 +0700

    (Perubahan pada 27/04/2024 fix)

--- pert4/Tugas/kalkulator.py
+++ pert4/Tugas/kalkulator.py
@@ -1,52 +1,37 @@
-def tambah(x, y):
-    return x + y
+def hitung(pilihan, value1, value2):
+    if pilihan == '1':
+        return tambah(value1, value2)
+    elif pilihan == '2':
+        return kurang(value1, value2)
+    elif pilihan == '3':
+        return kali(value1, value2)
+    elif pilihan == '4':
+        return bagi(value1, value2)
+    
+def tampilan_info():
+    print("Operasi:")
+    print("1.Tambah")
+    print("2.Kurang")
+    print("3.Kali")
+    print("4.Bagi")
+    print("5.Keluar")
 
-def kurang(x, y):
-    return x - y
-
-def kali(x, y):
-    return x * y
-
-def bagi(x, y):
-    if y != 0:
-        return x / y
-    else:
-        return "Error: Pembagian dengan nol"
 
 def main():
     while True:
-        print("Operasi:")
-        print("1. Tambah")
-        print("2. Kurang")
-        print("3. Kali")
-        print("4. Bagi")
-        print("5. Keluar")
-
         pilihan = input("Pilih operasi (1/2/3/4/5): ")
 
         if pilihan in ('1', '2', '3', '4'):
-            num1 = float(input("Masukkan angka pertama: "))
-            num2 = float(input("Masukkan angka kedua: "))
-
-            if pilihan == '1':
-                print("Hasil: ", tambah(num1, num2))
-
-            elif pilihan == '2':
-                print("Hasil: ", kurang(num1, num2))
-
-            elif pilihan == '3':
-                print("Hasil: ", kali(num1, num2))
-
-            elif pilihan == '4':
-                print("Hasil: ", bagi(num1, num2))
-        
-            elif pilihan == '5':
-            print("Keluar dari program...")
+            value1 = float(input("Masukkan angka pertama: "))
+            value2 = float(input("Masukkan angka kedua: "))
+            hasil = hitung(pilihan, value1, value2)
+            print("Hasil: ", hasil)
+        elif pilihan == '5':
+            print("Quit")
             break
-        
-            else:
-            print("Pilihan tidak valid!")
+        else:
+            print("Pilihan tidak valid!")        
 
 
 if __name__ == "__main__":
-    main()
+    main()            

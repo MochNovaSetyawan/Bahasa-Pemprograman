53237b0b41e0fd96a7349eca5c9d2399c2b932ec
root <root@Chrome>
2024-05-11 09:21:10 +0700

    (Perubahan pada 11/05/2024)

--- pert6/tugas/nomor2.py
+++ pert6/tugas/nomor2.py
@@ -15,22 +15,19 @@ def definisi_menu():
     }
     return menu_teh, menu_kopi
 
-def main():
-    menu_teh, menu_kopi = definisi_menu()
-
+def function_pilihMenu(menu_teh, menu_kopi):
     while True:
         pilihan = input("Pilih minuman Anda (Teh/Kopi): ").lower()
         if pilihan == "teh":
             tampilkan_menu(menu_teh)
-            menu_dipilih = menu_teh
-            break
+            return menu_teh
         elif pilihan == "kopi":
             tampilkan_menu(menu_kopi)
-            menu_dipilih = menu_kopi
-            break
+            return menu_kopi
         else:
             print("Pilihan tidak valid. Silakan pilih antara 'Teh' atau 'Kopi'.")
 
+def function_pilihListMenu(menu_dipilih):
     total_harga = 0
     while True:
         pilihan_item = int(input("Pilih nomor menu yang diinginkan (atau 0 untuk selesai): "))
@@ -42,7 +39,12 @@ def main():
             total_harga += jumlah * menu_dipilih[item_terpilih]
         else:
             print("Pilihan tidak valid. Silakan pilih nomor yang sesuai dengan menu.")
+    return total_harga
 
+def main():
+    menu_teh, menu_kopi = definisi_menu()
+    menu_dipilih = function_pilihMenu(menu_teh, menu_kopi)
+    total_harga = function_pilihListMenu(menu_dipilih)
     print(f"Total yang harus dibayar: Rp{total_harga}")
 
 if __name__ == "__main__":
